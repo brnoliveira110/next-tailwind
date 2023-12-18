a29fc9696fa261115b49ecabd9f1975e8a73bdf2
Caio Santana <brnoliveirasantana@gmail.com>
2023-12-18 07:45:57 -0300

    Inicio da criação do formulário

--- src/components/SettingsTabs/TabItem.tsx
+++ src/components/SettingsTabs/TabItem.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React from 'react'
 import * as Tabs from "@radix-ui/react-tabs";
+import { motion } from 'framer-motion';
 
 export interface TabItemProps {
     value: string
@@ -12,9 +13,18 @@ export const TabItem = ({ value, title, isSelected = false }: TabItemProps) => {
     return (
         <Tabs.Trigger
             value={value}
-            className="px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700"
+            className="relative px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700 data-[state=active]:text-violet-700"
         >
             <span>{title}</span>
+
+            {isSelected && (
+                <motion.div
+                    layoutId="activeTab"
+                    className='absolute -bottom-px left-0 right-0 h-0.5 bg-violet-700'
+                />
+            )
+
+            }
         </Tabs.Trigger>
     )
 }
